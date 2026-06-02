7edb2d98e1b396403484c63c6b3a4d8c7555dcc5
bvijay <bvijaykrishnabvk@gmail.com>
2026-06-02 11:48:30 +0530

    Time: 5 ms (97.29%), Space: 44.9 MB (94.49%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,31 +1,31 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Deque<String> d = new ArrayDeque<>();
-        int a =0;
-        int b =0;
+        Deque<Integer> d = new ArrayDeque<>();
+        int a = 0;
+        int b = 0;
         for(String s : tokens){
             switch(s){
                 case "+":
-                d.push(Integer.parseInt(d.pop()) + Integer.parseInt(d.pop())+"");
+                d.push(d.pop() + d.pop());
                 break;
                 case "-":       
-                a  = Integer.parseInt(d.pop());
-                b  = Integer.parseInt(d.pop());
-                d.push((b-a) + "");
+                a = d.pop();
+                b = d.pop();
+                d.push(b - a);
                 break;
                 case "/":
-                a= Integer.parseInt(d.pop());
-                b  = Integer.parseInt(d.pop());
-                d.push((b /a) + "");
+                a = d.pop();
+                b = d.pop();
+                d.push(b / a);
                 break;
                 case "*":
-                d.push(Integer.parseInt(d.pop()) * Integer.parseInt(d.pop())+"");
+                d.push(d.pop() * d.pop());
                 break;
                 default:
-                d.push(s);
+                d.push(Integer.parseInt(s));
                 break;
             }
         }
-        return Integer.parseInt(d.peek());
+        return d.peek();
     }
 }
